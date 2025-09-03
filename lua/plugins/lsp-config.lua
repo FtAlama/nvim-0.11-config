cabfe3198e6a2730b095e60527f4d48716def735
alama <55560656+FtAlama@users.noreply.github.com>
2025-09-04 00:58:52 +0200

    nvim step up

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,43 @@
+return {
+	{
+		"mason-org/mason.nvim",
+		opt = {},
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"clangd",
+					"vimls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities
+			})
+			lspconfig.vimls.setup({
+				capabilities = capabilities
+			})
+
+			vim.keymap.set("n", "D", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "I", vim.lsp.buf.implementation, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>l", vim.lsp.buf.references, {})
+		end,
+	},
+}
