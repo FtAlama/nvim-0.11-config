1984ccc1fc26d2e9b1c3e3d9e03fac4bd31255a0
Ajit <55560656+FtAlama@users.noreply.github.com>
2025-12-01 15:59:27 +0100

    [lsp-config & none-ls] : deprecated lsp-config fixed and working on some formatter for html css

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,22 @@
 return {
 	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
-
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.diagnostics.cpplint,
 				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.formatting.erb_formatter,
+				null_ls.builtins.formatting.prettier,
 			},
 		})
-		vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+		
+		vim.keymap.set("n", "<leader>f", function()
+			vim.lsp.buf.format({ async = false })
+		end, {})
 	end,
 }
