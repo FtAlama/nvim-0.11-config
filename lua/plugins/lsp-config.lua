a84e38d25e520824a0dadf8bb68af3611f64f24e
Ajit <55560656+FtAlama@users.noreply.github.com>
2025-12-01 16:57:22 +0100

    [lsp-config & none-ls] : rollback lsp-config cause none-ls does not handle new lsp api, still silenty remove lsp deprecated error

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,3 +1,78 @@
+-- return {
+-- 	{
+-- 		"williamboman/mason.nvim",
+-- 		lazy = false,
+-- 		config = function()
+-- 			require("mason").setup()
+-- 		end,
+-- 	},
+-- 	{
+-- 		"williamboman/mason-lspconfig.nvim",
+-- 		lazy = false,
+-- 		dependencies = { "williamboman/mason.nvim" },
+-- 		config = function()
+-- 			local mason_lspconfig = require("mason-lspconfig")
+--
+-- 			mason_lspconfig.setup({
+-- 				ensure_installed = {
+-- 					"lua_ls",
+-- 					"clangd",
+-- 					"vimls",
+-- 					"html",
+-- 					"cssls",
+-- 				},
+-- 			})
+--
+-- 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+--
+-- 			local on_attach = function(client, bufnr)
+-- 				local map = function(mode, lhs, rhs)
+-- 					vim.keymap.set(mode, lhs, rhs, { buffer = bufnr })
+-- 				end
+--
+-- 				map("n", "D", vim.lsp.buf.hover)
+-- 				map("n", "gd", vim.lsp.buf.definition)
+-- 				map("n", "I", vim.lsp.buf.implementation)
+-- 				map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action)
+-- 				map("n", "<leader>l", vim.lsp.buf.references)
+-- 			end
+--
+-- 			vim.lsp.config("lua_ls", {
+-- 				capabilities = capabilities,
+-- 				on_attach = on_attach,
+-- 				settings = {
+-- 					Lua = {
+-- 						diagnostics = {
+-- 							globals = { "vim" }
+-- 						}
+-- 					}
+-- 				}
+-- 			})
+--
+-- 			vim.lsp.config("clangd", {
+-- 				capabilities = capabilities,
+-- 				on_attach = on_attach,
+-- 			})
+--
+-- 			vim.lsp.config("vimls", {
+-- 				capabilities = capabilities,
+-- 				on_attach = on_attach,
+-- 			})
+--
+-- 			vim.lsp.config("html", {
+-- 				capabilities = capabilities,
+-- 				on_attach = on_attach,
+-- 			})
+--
+-- 			vim.lsp.config("cssls", {
+-- 				capabilities = capabilities,
+-- 				on_attach = on_attach,
+-- 			})
+--
+-- 			vim.lsp.enable({ "lua_ls", "clangd", "vimls", "html", "cssls" })
+-- 		end,
+-- 	},
+-- }
 return {
 	{
 		"williamboman/mason.nvim",
@@ -37,39 +112,27 @@ return {
 				map("n", "<leader>l", vim.lsp.buf.references)
 			end
 			
-			vim.lsp.config("lua_ls", {
-				capabilities = capabilities,
-				on_attach = on_attach,
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" }
-						}
-					}
-				}
-			})
+			local notify = vim.notify
+			vim.notify = function(msg, ...)
+				if msg:match("deprecated") or msg:match("lspconfig") then
+					return
+				end
+				notify(msg, ...)
+			end
 			
-			vim.lsp.config("clangd", {
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
+			local lspconfig = require("lspconfig")
 			
-			vim.lsp.config("vimls", {
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
-			
-			vim.lsp.config("html", {
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
-			
-			vim.lsp.config("cssls", {
-				capabilities = capabilities,
-				on_attach = on_attach,
-			})
-			
-			vim.lsp.enable({ "lua_ls", "clangd", "vimls", "html", "cssls" })
+			for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
+				if lspconfig[server] then
+					lspconfig[server].setup({
+						capabilities = capabilities,
+						on_attach = on_attach,
+					})
+				end
+			end
 		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
 	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,11 +10,11 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.diagnostics.cpplint,
 				null_ls.builtins.formatting.clang_format,
-				null_ls.builtins.formatting.erb_formatter,
 				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.erb_formatter,
 			},
 		})
-		
+
 		vim.keymap.set("n", "<leader>f", function()
 			vim.lsp.buf.format({ async = false })
 		end, {})
